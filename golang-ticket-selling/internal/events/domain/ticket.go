6ed632144a5fb921e9d9375117ec688f5bec0614
Ives M. W <ivesmw@gmail.com>
2024-08-04 18:11:32 -0300

    feat: add validation for ticket price and type

--- golang-ticket-selling/internal/events/domain/ticket.go
+++ golang-ticket-selling/internal/events/domain/ticket.go
@@ -1,6 +1,12 @@
 package domain
 
+import "errors"
+
 type TicketType string
+
+var (
+	ErrTicketPriceZero = errors.New("ticket price must be greater than 0")
+)
 
 const (
 	TicketTypeHalf TicketType = "half" // Half-price ticket
@@ -13,4 +19,21 @@ type Ticket struct {
 	Spot       *Spot
 	TicketType TicketType
 	Price      float64
+}
+
+func IsValidTicketType(ticketType TicketType) bool {
+	return ticketType == TicketTypeHalf || ticketType == TicketTypeFull
+}
+
+func (t *Ticket) CalculatePrice() {
+	if t.TicketType == TicketTypeHalf {
+		t.Price /= 2
+	}
+}
+
+func (t *Ticket) Validate() error {
+	if t.Price <= 0 {
+		return ErrTicketPriceZero
+	}
+	return nil
 }
