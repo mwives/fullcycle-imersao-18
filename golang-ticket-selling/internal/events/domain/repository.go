629537c66f925498f5453e106ef145a9fc0c3247
Ives M. W <ivesmw@gmail.com>
2024-08-04 18:15:08 -0300

    feat: add EventRepository interface

---
+++ golang-ticket-selling/internal/events/domain/repository.go
@@ -0,0 +1,13 @@
+package domain
+
+type EventRepository interface {
+	ListEvents() ([]Event, error)
+	FindEventByID(eventID string) (*Event, error)
+	FindSpotsByEventID(eventID string) ([]*Spot, error)
+	FindSpotByName(eventID, spotName string) (*Spot, error)
+	// TODO: Implement the following methods
+	// CreateEvent(event *Event) error
+	// CreateSpot(spot *Spot) error
+	// CreateTicket(ticket *Ticket) error
+	ReserveSpot(spotID, ticketID string) error
+}
