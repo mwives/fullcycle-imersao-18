5fa6caabda8493c7125271388170710a3e35ddf7
Ives M. W <ivesmw@gmail.com>
2024-08-07 22:01:35 -0300

    feat: add home page and its components

---
+++ nextjs-frontend/src/models.ts
@@ -0,0 +1,16 @@
+export type EventModel = {
+  id: string;
+  name: string;
+  organization: string;
+  date: string;
+  price: number;
+  rating: string;
+  imageUrl: string;
+  location: string;
+};
+
+export type SpotModel = {
+  id: string;
+  name: string;
+  status: string;
+};
